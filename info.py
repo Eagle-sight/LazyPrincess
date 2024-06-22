6d4fa9aeeae2733301b7e5e334363531f5b9f48d
Bharat jaiswal <bkj2453@gmail.com>
2024-06-22 17:34:59 +0545

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Autofilter2")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002225933814'))
 LAZY_GROUP_LOGS = int(environ.get('LAZY_GROUP_LOGS', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'LazyDeveloper')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
