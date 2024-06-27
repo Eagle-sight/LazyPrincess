dc7032bc46f3c38cb83449e497d7d318c3f1b027
Bharat jaiswal <bkj2453@gmail.com>
2024-06-27 11:26:26 +0545

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -433,9 +433,31 @@ async def settings(client, message):
 
     settings = await get_settings(grp_id)
 
-    if settings is not None:
+    try:
+        if settings['max_btn']:
+            settings = await get_settings(grp_id)
+    except KeyError:
+    #    await save_group_settings(grp_id, 'fsub', None)
+        await save_group_settings(grp_id, 'max_btn', False)
+        settings = await get_settings(grp_id)
+    if 'is_shortlink' not in settings.keys():
+        await save_group_settings(grp_id, 'is_shortlink', False)
+    else:
+        pass
+
+  if settings is not None:
         if message.from_user.id in ADMINS:
-            buttons = [
+        buttons = [
+            [
+                InlineKeyboardButton(
+                    'Rᴇsᴜʟᴛ Pᴀɢᴇ',
+                    callback_data=f'setgs#button#{settings["button"]}#{grp_id}',
+                ),
+                InlineKeyboardButton(
+                    'Bᴜᴛᴛᴏɴ' if settings["button"] else 'Tᴇxᴛ',
+                    callback_data=f'setgs#button#{settings["button"]}#{grp_id}',
+                ),
+            ],
             [
                 InlineKeyboardButton(
                     'Filter Button',
@@ -499,6 +521,16 @@ async def settings(client, message):
         ]
         else:
             buttons = [
+            [
+                InlineKeyboardButton(
+                    'Rᴇsᴜʟᴛ Pᴀɢᴇ',
+                    callback_data=f'setgs#button#{settings["button"]}#{grp_id}',
+                ),
+                InlineKeyboardButton(
+                    'Bᴜᴛᴛᴏɴ' if settings["button"] else 'Tᴇxᴛ',
+                    callback_data=f'setgs#button#{settings["button"]}#{grp_id}',
+                ),
+            ],
             [
                 InlineKeyboardButton(
                     'Filter Button',
